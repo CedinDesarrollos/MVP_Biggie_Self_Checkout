f93c353eb687082e38a0e895be647968809f683b
CedinDesarrollos <desarrollos@cedin.com.py>
2025-05-04 19:25:11 -0300

    Funcionalidad de Camara con Ngrok funcional

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
-# Inicializa la app Flask
+import os
 from flask import Flask
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()  # Carga las variables desde .env
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    load_dotenv()
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
     from .routes.main import main_bp
     app.register_blueprint(main_bp)
+
     return app
--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
-# Aquí puedes definir tus modelos si usas base de datos más adelante
+from . import db
+
+class Producto(db.Model):
+    __tablename__ = 'productos'
+
+    upc = db.Column(db.String, primary_key=True)
+    product_name = db.Column(db.String)
+    price = db.Column(db.Float)
+    weight = db.Column(db.String)
+    tax_percentage = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Producto {self.product_name}>'
